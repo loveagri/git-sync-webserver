e22d23cef19964cf2fa6e0614c24b855c98159a0
tfh <282656050@qq.com>
2018-05-20 18:41:07 +0800

    hdxj

---
+++ webhook.php
@@ -0,0 +1,19 @@
+<?php
+
+$secret = 'houdunren';
+
+$path = 'home/wwwroot/default';
+
+
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
+if ($signature){
+	$hash = "sha1" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
+	if (strcmp($signature,$hash) == 0) {
+		echo shell_exec("cd {$path} && /usr/bin/git rest --hard origin/master && /usr/bin/git/ clean -f && /usr/bin/git pull 2>&1");
+		exit();
+	}
+}
+
+http_response_code(404);
+?>
